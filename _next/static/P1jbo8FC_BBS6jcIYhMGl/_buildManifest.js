fc0c3b8920419aff2b7478f769e0cd1d1bc0a079
Raul Kele <raulkeleblk@gmail.com>
2022-06-14 19:40:39 +0300

    Deploy gh-pages

--- _next/static/P1jbo8FC_BBS6jcIYhMGl/_buildManifest.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:out/_next/static/bAVdzxhbsRRePbNoJLM1o/_buildManifest.js
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/fd186abcaa42aad9.css","static/chunks/pages/index-ab73b27288fae88d.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],"/discover":["static/chunks/pages/discover-85c2cfeca2c237da.js"],sortedPages:["/","/_app","/_error","/discover"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
-========
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/6194dee992a9d0cb.css","static/chunks/pages/index-3bdc1ed3128811ef.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],"/discover":["static/chunks/pages/discover-85c2cfeca2c237da.js"],sortedPages:["/","/_app","/_error","/discover"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
->>>>>>>> 0aee34018eac7e739355821b6cdaf20b64c0499c:out/_next/static/P1jbo8FC_BBS6jcIYhMGl/_buildManifest.js
