45f414765563fd7e6970abb22d30e05866c8d900
Raul Kele <raulkeleblk@gmail.com>
2022-06-14 14:21:53 +0300

    config work and clearing out folder

--- next.config.js
+++ next.config.js
@@ -1,22 +1,22 @@
-const repoNameURIPrefix =
-  process.env.NODE_ENV === 'production' ? '/zot-site' : '';
+const repoNameURIPrefix = '/zot-site';
+//   process.env.NODE_ENV === 'production' ? '/zot-site' : '';
 
 module.exports = {
     basePath: repoNameURIPrefix,
     assetPrefix:repoNameURIPrefix,
-    trailingSlash:true,
+    trailingSlash: true,
     images: {
         loader: "custom",
         imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
         deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
         nextImageExportOptimizer: {
             imageFolderPath: "public",
-            exportFolderPath: "out",
+            exportFolderPath: "out/zot-site",
             quality: 75,
         },
     },
     env: {
-        storePicturesInWEBP: true,
+        storePicturesInWEBP: false,
         generateAndUseBlurImages: true,
     }
 }
