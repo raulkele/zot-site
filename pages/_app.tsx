0e106157d6f84b6dd440608fe470bc1bb1d7ce3b
Raul Kele <raulkeleblk@gmail.com>
2022-08-03 16:15:33 +0300

    configure mui v5 for production
    Signed-off-by: Raul Kele <raulkeleblk@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,29 @@
-import Layout from '@/components/Layout'
-import '@/styles/globals.scss'
+import Layout from "@/components/Layout";
+import Meta from "@/components/Meta";
+import "@/styles/globals.scss";
+import { CacheProvider } from "@emotion/react";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import createEmotionCache from '../utils/createEmotionCache';
+import theme from '../utils/theme';
 
-function MyApp({ Component, pageProps }) {
+// no default modifications for mui theme, custom theme provided with scss modules 
+const clientSideEmotionCache = createEmotionCache();
+
+function MyApp(props) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
+    <CacheProvider value={emotionCache}>
+      <Meta />
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
