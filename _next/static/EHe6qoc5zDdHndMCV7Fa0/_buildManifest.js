73443d1df1c4926ed84ddc67146cc8df8dcc3f8e
Raul Kele <raulkeleblk@gmail.com>
2022-08-04 17:25:54 +0300

    Deploy gh-pages

---
+++ _next/static/EHe6qoc5zDdHndMCV7Fa0/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/afc5c4afcd4b01ae.css","static/chunks/pages/index-1699a95675824797.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
